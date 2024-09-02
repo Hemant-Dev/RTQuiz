cbbe9ed5a9561afcd870a9ec34e6f5160392334e
Hemant-Dev <phemant092@gmail.com>
2024-09-02 18:28:40 +0530

    started creating crud for UserAnswer

---
+++ RTQuiz.IServices/IUserAnswerService.cs
@@ -0,0 +1,14 @@
+﻿using RTQuiz.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTQuiz.IServices
+{
+    public interface IUserAnswerService
+    {
+        Task<IEnumerable<GetUserAnswerDTO>> GetAll();
+    }
+}
---
+++ RTQuiz.Repositories/UserAnswerRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RTQuiz.Data;
+using RTQuiz.IRepositories;
+using RTQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTQuiz.Repositories
+{
+    public class UserAnswerRepository : BaseRepository<UserAnswer>, IUserAnswerRepository
+    {
+        private readonly QuizDBContext _quizDBContext;
+        public UserAnswerRepository(QuizDBContext quizDBContext) : base(quizDBContext)
+        {
+            _quizDBContext = quizDBContext;   
+        }
+
+        public Task<IEnumerable<UserAnswer>> GetAllUserAnswerWithOtherData()
+        {
+            var answerList = _quizDBContext.UserAnswers.Include()
+        }
+    }
+}
